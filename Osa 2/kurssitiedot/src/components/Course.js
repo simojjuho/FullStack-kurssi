dfc7700849b7228d19ba98a21d66ae27c98d8064
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-11-02 13:23:25 +0200

    Kurssitiedot stepit 2.1-2.5

--- Osa 2/kurssitiedot/src/components/Course.js
+++ Osa 2/kurssitiedot/src/components/Course.js
@@ -1,43 +1,41 @@
-const Course = (props) => {
-    return ( 
-        <>
-            <Header course={props.course.name} />
-            <Content courses = {props.course} />
-            <Total parts = {props.course.parts} />
-        </>
-    )
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content courses={course} />
+      <Total parts={course.parts} />
+    </>
+  )
 }
 
 const Header = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )
-  }
-  
-  const Content = ({courses}) => {
-    return (
-      <ul>
-        {courses.parts.map(part => 
-        <Part key={part.id} name = {part.name} points={part.exercises}/>
-        )}
-      </ul>
-    )
-  }
-  
-  const Total = ({parts}) => {
-    let totalExercises = 0
-    for (let part of parts) {
-      totalExercises += part.exercises
-    }
-    return (
-      <p>Number of exercises: {totalExercises}</p>
-    )
-  }
-  
-  const Part = ({name, points}) => {
-    return (
-      <li>{name}: {points}</li>
-    )
-  }
+  return (
+    <h2>{props.course}</h2>
+  )
+}
 
-  export default Course
+const Content = ({ courses }) => {
+  return (
+    <ul>
+      {courses.parts.map(part =>
+        <Part key={part.id} name={part.name} points={part.exercises} />
+      )}
+    </ul>
+  )
+}
+
+const Total = ({ parts }) => {
+  let initialValue = 0
+  const total = parts.reduce((total, current) => total + current.exercises, initialValue)
+  return (
+    <p style={{fontWeight: 'bold'}}>Number of exercises: {total}</p>
+  )
+}
+
+const Part = ({ name, points }) => {
+  return (
+    <li>{name}: {points}</li>
+  )
+}
+
+export default Course
