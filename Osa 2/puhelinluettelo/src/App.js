ef70c8f70776379af1fae8a91767bc2950a95ecc
Juho Simojoki <juho.simojoki@outlook.com>
2022-11-04 11:09:43 +0200

    Numeroiden näyttö omaan komponenttiinsa

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -1,29 +1,35 @@
 import { useState } from 'react'
+import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 1 }
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
     const contactObject = {
       id: persons.length + 1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.some(element => element.name === newName)) {
       alreadyAdded(contactObject.name)
     } else {
       setPersons(persons.concat(contactObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleNameFieldChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameFieldChange = (event) => setNewName(event.target.value)
+  
+  const handleNumberFieldChange = (event) => setNewNumber(event.target.value)
 
   const alreadyAdded = (person) => alert(`${person} already added to the contact list!`)
 
@@ -35,23 +41,23 @@ const App = () => {
           name: <input 
                   value={newName}
                   onChange={handleNameFieldChange} 
+                /><br />
+          number: <input
+                  value={newNumber}
+                  onChange={handleNumberFieldChange}
                 />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person =>
-      <Person key={person.id} person={person.name}/>
-      )}
+      <div>
+        <Numbers numbers={persons}/>
+      </div>
     </div>
+      
   )
 
 }
 
 export default App
-
-const Person = ({person}) => {
-  return <p>{person}</p>
-}
