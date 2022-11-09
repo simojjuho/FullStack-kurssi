e6f394d1a87367a1faca4b96e3688b0445d0ed27
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-11-09 14:48:39 +0200

    Puhelinluettelossa voi nyt myös päivittää nykyisiä yhteystietoja.

---
+++ Osa 2/puhelinluettelo/src/services/numbers.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const createContact = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const updateContact = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+    
+}
+
+const deleteContact = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const update = (id, updatedContact) => {
+    const request = axios.put(`${baseUrl}/${id}`, updatedContact)
+    return request.then(response => response.data)
+}
+
+export default {
+    getAll,
+    createContact,
+    updateContact,
+    deleteContact,
+    update
+}
