8d77122e9603b0f9cc45b10ec0639599ab3bc450
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-12-04 16:05:59 +0200

    Sirretty Osa3 kansion sisältö Osa4 kansioon

---
+++ Osa4/blogilista-backend/utils/middleware.js
@@ -0,0 +1,57 @@
+const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+
+//According to the name logs all the requests on console
+const requestLogger = (request, response, next) => {
+  logger.info('Method: ', request.method)
+  logger.info('Path: ', request.path)
+  logger.info('Body: ', request.body)
+  logger.info('----')
+  next()
+}
+
+//Error handling middleware taking care of all the errors in one place
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.nessage)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send( { error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send( { error: error.nessage })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).send({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).send({ error: 'token expired' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const token = authorization.substring(7)
+    request.token = token
+  }
+
+  next()
+}
+
+const userExtractor = (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  request.user = decodedToken
+  next()
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+  userExtractor
+
+}
