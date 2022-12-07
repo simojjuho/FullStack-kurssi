50896753bc2128c957bd4db7a7696cb4f36bc553
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-12-07 14:19:47 +0200

    UI:lla on myt mahdollisuus lisätä uusia blogeja. t. 5.3 asti

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -9,6 +9,9 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMsg, setErrorMsg] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
 
   useEffect(() => {
@@ -17,17 +20,57 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem('loggedUser')
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
     console.log('Logging in with', username, password)
-    try{
+    try {
       const user = await loginService.login({ username, password })
       setUser(user)
-      console.log(user)
+      blogService.setToken(user.token)
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
       setUsername('')
       setPassword('')      
     } catch (error) {
       setErrorMsg('unauthorized')
+      setTimeout(() => {
+        setErrorMsg(null)
+      }, 5000)
+    }
+  }
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
+  const handleCreate = async event => {
+    event.preventDefault()
+    const newBlog = {
+      title,
+      author,
+      url
+    }
+    console.log(`Creating a blog ${newBlog.toString()}`)
+    try {
+      const result = await blogService.create(newBlog)
+      console.log(result)
+      setAuthor('')
+      setTitle('')
+      setUrl('')
+      blogService.getAll().then(blogs =>
+        setBlogs( blogs )
+      )  
+    } catch (exeption) {
+      setErrorMsg(exeption.error)
       setTimeout(() => {
         setErrorMsg(null)
       }, 5000)
@@ -62,11 +105,35 @@ const App = () => {
       )
     }
     return (
-      <div>      
+      <div>
+        <p>{user.name} logged in</p> 
+        <button onClick={()=>logout()}>logout</button>
         <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
+        <h2>Create a blog</h2>
+        <form onSubmit={handleCreate}>
+          <input 
+            type='text'
+            value={title}
+            name='Title'
+            onChange={({ target })=> setTitle(target.value)}
+          />
+          <input
+            type='text'
+            value={author}
+            name='Author'
+            onChange={({ target }) => setAuthor( target.value )}
+            />
+          <input
+            type='text'
+            value={url}
+            name='Url'
+            onChange={({ target }) => setUrl( target.value )}
+          />
+          <button type='submit'>create</button>
+        </form>
       </div>
     )
   }
--- Osa5/bloglist-frontend/src/services/blogs.js
+++ Osa5/bloglist-frontend/src/services/blogs.js
@@ -1,13 +1,29 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = async () => {
-  const response = await axios.get(baseUrl)
-  console.log(response)
-  return response.user
-  
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
 }
 
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  console.log(`Status: ${response.status}, data:`)
+  console.log(response.data)
+  return response.data
+}
+
+const create = async newBlog => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.post(baseUrl, newBlog, config)
+  return response.data
+} 
+
 export default { 
-  getAll
+  getAll, 
+  create,
+  setToken
 }
