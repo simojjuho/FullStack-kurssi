0a5fdf1c554803e1a40b121a1ce502d61fb6aab3
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-12-09 18:38:53 +0200

    Assignment 5.7, refactored the blog listing.

--- Osa5/bloglist-frontend/src/components/Blog.js
+++ Osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [showMore, setShowMore] = useState(false)
+  
+  const toggleVisibility = () => {
+    setShowMore(!showMore)
+  }
+
+  const blogStyle = {
+    border: '2px black solid',
+    maxWidth:  '250px',
+    padding: '10px',
+    marginBottom: '5px'
+  }
+
+
+
+  if(showMore) {
+    return (
+      <div style={blogStyle}>
+        Title: {blog.title}<br />
+        Author: {blog.author}<br />
+        Likes: {blog.likes}<br />
+        <button>like</button><br />
+        Url: {blog.url}<br />
+        <button onClick={() => toggleVisibility()}>view less</button>
+      </div>
+    )
+  } else {
+    return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={() => toggleVisibility()}>view more</button>
+    </div>  
+    )
+  }
+}
+
 
 export default Blog
