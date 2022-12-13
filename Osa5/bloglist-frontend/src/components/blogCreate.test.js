df9bd2b8ffe4f9e05f58d164f007861d80a86969
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-12-13 17:34:00 +0200

    Assignment 5.16 done
    Checking that the function taking care of the blog creation is called once and it has all the parametres given. I had to use async await to wait for the test to type the input contents.

---
+++ Osa5/bloglist-frontend/src/components/blogCreate.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import CreateBlogForm from './CreateBlogForm'
+
+test('createBlog is called with proper arguments', async () => {
+  const handleCreate = jest.fn()
+
+  render(<CreateBlogForm handleCreate={handleCreate}/>)
+
+  const inputAuthor = screen.getByPlaceholderText('blog author')
+  const inputTitle = screen.getByPlaceholderText('blog title')
+  const inputUrl = screen.getByPlaceholderText('blog url')
+  const sendButton = screen.getByText('create')
+
+  await userEvent.type(inputAuthor, 'Niinistö')
+  await userEvent.type(inputTitle, 'title')
+  await userEvent.type(inputUrl, 'url.com')
+  await userEvent.click(sendButton)
+
+  expect(handleCreate.mock.calls).toHaveLength(1)
+  expect(handleCreate.mock.calls[0][0].author).toBe('Niinistö')
+})
