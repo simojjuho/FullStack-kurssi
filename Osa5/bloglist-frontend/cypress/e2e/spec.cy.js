52331da79579b13ae9304bc1a8235b8e09fa2eb7
Juho Simojoki <juho.simojoki@outlook.com>
2022-12-16 17:23:54 +0200

    Assignments 5.19-5.21 done.
    Tests now, that blogs can be created, likes and removed.

--- Osa5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ Osa5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -17,17 +17,58 @@ describe('Blog app', function() {
     cy.get('#loginVisible')
   })
 
-  it('login succesful', function() {
-    cy.get('#username').type('root')
-    cy.get('#password').type('secret')
-    cy.get('#loginSubmit').click()
-    cy.contains('root logged in')
+  describe('Login', function() {
+    it('login succesful', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('secret')
+      cy.get('#loginSubmit').click()
+      cy.contains('root logged in')
+    })
+
+    it('not succesful with wrong credentials', function() {
+      cy.get('#username').type('juho')
+      cy.get('#password').type('secret')
+      cy.get('#loginSubmit').click()
+      cy.contains('Unauthorized, wrong username and/or password')
+    })
   })
 
-  it('not succesful with wrong credentials', function() {
-    cy.get('#username').type('juho')
-    cy.get('#password').type('secret')
-    cy.get('#loginSubmit').click()
-    cy.contains('Unauthorized, wrong username and/or password')
+
+  describe('When logged in', function() {
+    beforeEach(function(){
+      cy.get('#username').type('root')
+      cy.get('#password').type('secret')
+      cy.get('#loginSubmit').click()
+    })
+    it('blog creation succesful', function() {
+      cy.get('#titleInput').type('Kekkonen lives!')
+      cy.get('#authorInput').type('A. Saarikko')
+      cy.get('#urlInput').type('kepu.com/')
+      cy.get('#createBlogButton').click()
+      cy.contains('Kekkonen lives! A. Saarikko')
+    })
+  })
+
+  describe('One blog created', function(){
+    beforeEach(function(){
+      cy.get('#username').type('root')
+      cy.get('#password').type('secret')
+      cy.get('#loginSubmit').click()
+      cy.get('#titleInput').type('Kekkonen lives!')
+      cy.get('#authorInput').type('A. Saarikko')
+      cy.get('#urlInput').type('kepu.com/')
+      cy.get('#createBlogButton').click()
+      cy.get('button.viewMoreButton').click()
+    })
+
+    it('pressing like adds one like', function(){
+      cy.get('button.addLike').click()
+      cy.contains('Likes: 1')
+    })
+
+    it('deletion possible for the one who created the blog', function(){
+      cy.get('.deleteButton').click()
+      cy.expect('#blogList').to.not.contain('Kekkonen lives!')
+    })
   })
 })
