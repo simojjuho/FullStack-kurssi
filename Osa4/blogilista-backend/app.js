325f32c9993daceb95c3a06e34538dd3064e65d0
Juho Simojoki <juho.simojoki@metropolia.fi>
2022-12-16 18:57:27 +0200

    Modified a bit backend in part four.
    Backend has a cypress compatible script using which it can be started in a test mode. Also a new reset route in test mode.

--- Osa4/blogilista-backend/app.js
+++ Osa4/blogilista-backend/app.js
@@ -30,6 +30,10 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+if ( process.env.NODE_ENV === 'test' ) {
+  const testRouter = require('./controllers/testRouter')
+  app.use('/api/testing', testRouter)
+}
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
