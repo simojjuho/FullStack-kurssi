513083c6fb3fe17991b8c51f0d5f4a8d19a72723
Juho Simojoki <juho.simojoki@outlook.com>
2023-01-27 09:45:27 +0200

    Assignment 6.13 done.
    Installed JSON server and started learning using redux toolkit with server side.

--- Osa6/redux-anecdotes/src/App.js
+++ Osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,20 @@
 import AnecodteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
-  
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then((response) => {
+        dispatch(setAnecdotes(response))        
+      })
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
